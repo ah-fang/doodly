8ab91c79f7b4aa10f1e002eb1036597117a74bc7
Andrea Fang <arhughes43@gmail.com>
2022-07-04 15:52:13 -0700

    login logic and clear button function

--- public/javascript/draw.js
+++ public/javascript/draw.js
@@ -1,9 +1,12 @@
 window.onload = () => {
-    const canvas = document.getElementById('canvas');
-    const saveButton = document.getElementById('save');
-    const loadInput = document.getElementById('load');
+  const canvas = document.getElementById('canvas');
+  const saveButton = document.getElementById('save');
+  const loadInput = document.getElementById('load');
 
-    new Drawing(canvas, saveButton, loadInput);
+  new Drawing(canvas, saveButton, loadInput);
+  document.querySelector("#save").addEventListener("click", saveBtnHandler);
+  // document.querySelector(".delete-btn").addEventListener("click", deleteBtnHandler);
+  document.querySelector("#clear").addEventListener("click", clearBtnHandler);
 };
 
 class Drawing {
@@ -68,3 +71,54 @@ class Drawing {
     })
   }
 }
+
+const clearBtnHandler = () => {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  console.log("the clear button was pressed");
+}
+const saveBtnHandler = async(event) => {
+  //save the drawing to a url and send all its relevant info to db in post request
+  event.preventDefault();
+  //*******add method to give post a title*******
+  const title = document.querySelector('input[name="post-title"]').value;
+  const drawing_url = document.querySelector('.canvas').toDataURL;
+
+  const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+          title, drawing_url
+      }),
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  });
+
+  if(response.ok) {
+      document.location.replace('/dashboard');
+      newPostBtn.setAttribute('style', 'display: block');
+  } else {
+      alert(response.statusText);
+  }
+}
+
+const deleteBtnHandler = async(event) => {
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE'
+  });
+
+  if(response.ok) {
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    document.location.replace('/dashboard');
+  } else {
+      alert(response.statusText);
+  }
+}
+
