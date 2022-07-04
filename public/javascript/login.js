f56856461de3689e3c1a95f4d75849873d30ddd4
Andrea Fang <arhughes43@gmail.com>
2022-07-04 14:25:01 -0700

    tags for login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,8 @@
 async function signupFormHandler(event) {
     event.preventDefault();
-    const username = document.querySelector('#username-signup').value.trim();
+    const username = document.querySelector('#un-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const password = document.querySelector('#pass-signup').value.trim();
 
     if (username && email && password) {
         const response = await fetch('/api/users', {
@@ -26,8 +26,8 @@ async function signupFormHandler(event) {
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#un-login').value.trim();
+    const password = document.querySelector('#pass-login').value.trim();
   
     if (email && password) {
         const response = await fetch('/api/users/login', {
